ee74b037abd065a6467838b706b64125b24e19f0
mahizaman490 <mahizaman490@gmail.com>
2023-08-20 23:08:31 +0600

    compleated the functions

--- script.js
+++ script.js
@@ -36,23 +36,73 @@ function enableButton() {
 enableButton();
 
 
+let selectedItemsCount = 0;
+let totalPrice = 0;
+let discount = 0;
+const cards = document.querySelectorAll('.card');
+const selectItemContainer = document.getElementById('selected-items');
+const totalSpan = document.querySelector('.font-medium.text-gray-500 > span');
+const discountSpan = document.querySelectorAll('.font-medium.text-gray-500 > span')[1];
+const finalTotalSpan = document.querySelectorAll('.font-medium.text-gray-500 > span')[2];
+const makePurchaseButton = document.querySelector('a.btn');
 
+// Function to update total prices
+function updateTotalPrices() {
+    totalSpan.innerText = totalPrice.toFixed(2) ;
+    discountSpan.innerText = discount.toFixed(2) ;
+    finalTotalSpan.innerText = (totalPrice - discount).toFixed(2) ;
+    
+    if (totalPrice > 0) {
+        makePurchaseButton.removeAttribute('disabled');
+    } else {
+        makePurchaseButton.setAttribute('disabled', 'true');
+    }
+}
 
+// Function to handle card click
+function handleClickCard(event) {
+    var h2 = this.querySelector('h2');
+    var price = this.querySelector('p span');
+    
+    if (h2 && price) {
+        selectedItemsCount++;
+        const li = document.createElement("li");
+        li.innerText = selectedItemsCount + ". " + h2.innerText;
+        selectItemContainer.appendChild(li);
+        totalPrice += parseFloat(price.innerText);
+        updateTotalPrices();
+    }
+}
 
+// Attach card click event listeners
+for (var i = 0; i < cards.length; i++) {
+    cards[i].addEventListener('click', handleClickCard);
+}
 
-// button enable er jonno
+function applyDiscount() {
+    const applyButton = document.getElementById('applyButton');
+    const couponInput = document.getElementById('couponInput');
+    
+    applyButton.addEventListener('click', function() {
+        if (couponInput.value === 'SELL200') {
+            discount = totalPrice * 0.2;
+            updateTotalPrices();
+        }
+    });
+}
 
+applyDiscount();
 
-// function enablePurchaseButton() {
-//     var totalPriceSpan = document.querySelector('.font-medium.text-gray-500 span');
-//     var purchaseButton = document.querySelector('.btn.ml-2.bg-pink-500.text-white.md:w-3/4.mx-auto');
+function resetPage() {
+    selectedItemsCount = 0;
+    totalPrice = 0;
+    discount = 0;
+    selectItemContainer.innerHTML = '';
+    updateTotalPrices();
+    document.getElementById('couponInput').value = '';
+    document.getElementById('applyButton').disabled = true;
+    makePurchaseButton.setAttribute('disabled', 'true');
+}
 
-//     function updateButtonState() {
-//         purchaseButton.disabled = parseFloat(totalPriceSpan.textContent.trim()) <= 0;
-//     }
-
-//     updateButtonState();
-//     totalPriceSpan.addEventListener('DOMSubtreeModified', updateButtonState);
-// }
-
-// enablePurchaseButton();
+// Attach reset event to modal button
+document.getElementById('my_modal_8').addEventListener('click', resetPage);
